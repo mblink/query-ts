324ee150d9f557fd691c8f85ee930eccecc418f2
Andrew Kiernan <andrew@bondlink.com>
2019-04-23 12:03:07 -0400

    Add string util file. Streamline getSrcO

--- util/concat.ts
+++
@@ -1,5 +0,0 @@
-export function concat(s1: string, s2: string): string;
-export function concat(s1: string): (s2: string) => string;
-export function concat(s1: string, s2o?: string): string | ((s2: string) => string) {
-  return typeof s2o === "string" ? s1 + s2o : (s2: string) => s1 + s2;
-}
