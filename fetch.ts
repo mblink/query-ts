b1a672aad8c8b202f9285ad84f73e0dbedaa13a9
Justin Leider <justin@leiders.org>
2019-08-14 17:24:25 -0400

    Add api wrapper and update fetchJson to pass along errors from parsing json

--- fetch.ts
+++ fetch.ts
@@ -1,17 +1,19 @@
 import { Bondlink } from "./bondlink";
 import { constant } from "fp-ts/lib/function";
 import { none, Option, some } from "fp-ts/lib/Option";
-import { chain, fromEither, fromPredicate, map, mapLeft, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
+import { chain, fromPredicate, fold as foldTE, map, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/pipeable";
 import * as iots from "io-ts";
 import { invoke0 } from "./util/invoke";
 import { mergeDeep } from "./util/merge";
 import { parseJson } from "./util/parseJson";
 import { prop } from "./util/prop";
+import { Either, fold as foldE, left, right } from "fp-ts/lib/Either";
+import { task } from "fp-ts/lib/Task";
 
 export type FetchResp = TaskEither<Option<Response>, Response>;
 export type FetchTextResp = TaskEither<Option<Response>, [Response, string]>;
-export type FetchJsonResp<A> = TaskEither<Option<Response>, [Response, A]>;
+export type FetchJsonResp<A> = TaskEither<Either<Option<Response>, iots.Errors>, [Response, A]>;
 
 const headers = mergeDeep({ headers: { "X-Requested-With": "XMLHttpRequest" } });
 const credentials = mergeDeep({ credentials: "same-origin" });
@@ -23,17 +25,25 @@ const origFetch = window.fetch;
 export const blFetch = (url: string, opts?: RequestInit): FetchResp =>
   chain(checkStatus)(tryCatch(() => origFetch(url, headers(credentials(opts || {}))), constant<Option<Response>>(none)));
 
-const fetchTpe = <A>(f: (r: Response) => Promise<A>) => (url: string, opts?: RequestInit): FetchJsonResp<A> =>
-  chain((res: Response) => map((a: A): [Response, A] => [res, a])(tryCatch(() => f(res), constant(some(res)))))(blFetch(url, opts));
+const fetchTpe = (f: (r: Response) => Promise<string>) => (url: string, opts?: RequestInit): FetchTextResp =>
+  chain((res: Response) => map((a: string): [Response, string] => [res, a])(tryCatch(() => f(res), constant(some(res)))))(blFetch(url, opts));
 
 export const fetchText = fetchTpe(invoke0("text"));
 
 export const fetchJson = <A, O, I>(tpe: iots.Type<A, O, I>) => (url: string, opts?: RequestInit): FetchJsonResp<iots.TypeOf<iots.Type<A, O, I>>> =>
-  chain(([res, s]: [Response, string]) => pipe(
-    fromEither(parseJson(tpe)(s)),
-    mapLeft(constant(some(res))),
-    map((a: A): [Response, A] => [res, a])
-  ))(fetchText(url, opts));
+  pipe(
+    fetchText(url, opts),
+    foldTE(
+      (or: Option<Response>) => task.of(left(left(or))),
+      ([res, s]: [Response, string]) => pipe(
+        parseJson(tpe)(s),
+        foldE(
+          (e: iots.Errors) => task.of(left(right(e))),
+          (a: A) => task.of(right([res, a]))
+        )
+      )
+    )
+  );
 
 export const postJson = (data: unknown) => (url: string, opts?: RequestInit): FetchResp =>
   blFetch(url, mergeDeep({
