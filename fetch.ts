117ecfb16c909d37ec6d1a8e5d02e77464b9e15e
Justin Leider <justin@leiders.org>
2019-08-20 17:39:31 -0400

    Improve strictness and consistency

--- fetch.ts
+++ fetch.ts
@@ -1,18 +1,26 @@
 import { Bondlink } from "./bondlink";
-import { none, Option, some } from "fp-ts/lib/Option";
-import { chain, fromPredicate, fold as foldTE, map, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
+import { none, Option, some, map } from "fp-ts/lib/Option";
+import {
+  chain as chainTE,
+  fromPredicate,
+  fold as foldTE,
+  TaskEither,
+  tryCatch as tryCatchTE,
+  map as mapTE,
+  mapLeft as mapLeftTE
+} from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/pipeable";
-import * as iots from "io-ts";
+import * as t from "io-ts";
 import { invoke0 } from "./util/invoke";
 import { mergeDeep } from "./util/merge";
 import { parseJson } from "./util/parseJson";
-import { prop } from "./util/prop";
 import { Either, fold as foldE, left, right } from "fp-ts/lib/Either";
 import { task } from "fp-ts/lib/Task";
 import { Log } from "./util/log";
 import { Method, UrlInterface } from "./routes/urlInterface";
+import { tap } from "./util/tap";
 
-export type RespOrErrors = Either<Option<Response>, iots.Errors>;
+export type RespOrErrors = Either<Option<Response>, t.Errors>;
 
 export type FetchResp = TaskEither<Option<Response>, Response>;
 export type FetchTextResp = TaskEither<Option<Response>, [Response, string]>;
@@ -20,49 +28,58 @@ export type FetchJsonResp<A> = TaskEither<RespOrErrors, [Response, A]>;
 
 const headers = mergeDeep({ headers: { "X-Requested-With": "XMLHttpRequest" } });
 const credentials = mergeDeep({ credentials: "same-origin" });
-
-const checkStatus = fromPredicate(prop("ok"), some);
-
+const checkStatus = fromPredicate((r: Response) => r.status < 300, some);
 const origFetch = window.fetch;
 
 export const blFetch = (url: UrlInterface<Method>, opts?: RequestInit): FetchResp =>
-  chain(checkStatus)(tryCatch(
-    () => origFetch(url.url, headers(credentials(Object.assign(opts || {}, {method: url.method})))),
-    (e: unknown) => {
-      Log.error("BLFetch Failed", e);
-      return none;
-    }
-  ));
+  pipe(
+    tryCatchTE(
+      () => origFetch(url.url, headers(credentials(Object.assign(opts || {}, {method: url.method})))),
+      (e: unknown) => {
+        Log.error("BLFetch Failed", e);
+        return none;
+      }
+    ),
+    chainTE(checkStatus),
+    mapLeftTE(map(tap((r: Response) => Log.error(`BLFetch Failed With Status: ${r.status} -- ${r.statusText}`, r))))
+  );
 
 const fetchTpe = (f: (r: Response) => Promise<string>) => (url: UrlInterface<Method>, opts?: RequestInit): FetchTextResp =>
-  chain((res: Response) =>
-    map((a: string): [Response, string] => [res, a])(tryCatch(
-      () => f(res),
-      (e: unknown) => {
-        Log.error("FetchTpe Failed", e);
-        return some(res);
-      })
-    ))(blFetch(url, opts));
+  pipe(
+    blFetch(url, opts),
+    chainTE((res: Response) =>
+      mapTE((a: string): [Response, string] => [res, a])(tryCatchTE(
+        () => f(res),
+        (e: unknown) => {
+          Log.error("FetchTpe Failed", e);
+          return some(res);
+        }
+      )
+    ))
+  );
 
 export const fetchText = fetchTpe(invoke0("text"));
 
-export const fetchJson = <A, O, I>(tpe: iots.Type<A, O, I>) => (url: UrlInterface<"GET">, opts?: RequestInit): FetchJsonResp<iots.TypeOf<iots.Type<A, O, I>>> =>
-  pipe(
-    fetchText(url, opts),
-    foldTE(
-      (or: Option<Response>) => task.of(left(left(or))),
-      ([res, s]: [Response, string]) => pipe(
-        parseJson(tpe)(s),
-        foldE(
-          (e: iots.Errors) => task.of(left(right(e))),
-          (a: A) => task.of(right([res, a]))
-        )
+export const textToJsonResp = <A, O, I>(tpe: t.Type<A, O, I>): (t: FetchTextResp) => FetchJsonResp<A> =>
+  foldTE(
+    (or: Option<Response>) => task.of(left(left(or))),
+    ([res, s]: [Response, string]) => pipe(
+      parseJson(tpe)(s),
+      foldE(
+        (e: t.Errors) => task.of(left(right(e))),
+        (a: A) => task.of(right([res, a]))
       )
     )
   );
 
-export const postJson = (data: unknown) => (url: UrlInterface<"POST">, opts?: RequestInit): FetchResp =>
-  blFetch(url, mergeDeep({
+export const fetchJson = <A, O, I>(tpe: t.Type<A, O, I>) => (url: UrlInterface<"GET">, opts?: RequestInit): FetchJsonResp<t.TypeOf<t.Type<A, O, I>>> =>
+  pipe(
+    fetchText(url, opts),
+    textToJsonResp(tpe)
+  );
+
+export const postJsonUnsafe = (data: unknown) => (url: UrlInterface<"POST">, opts?: RequestInit): FetchTextResp =>
+  fetchText(url, mergeDeep({
     cache: "no-cache",
     body: JSON.stringify(data),
     headers: {
@@ -70,3 +87,5 @@ export const postJson = (data: unknown) => (url: UrlInterface<"POST">, opts?: Re
       "Content-Type": "application/json"
     }
   })(opts || {}));
+
+
--- util/log.ts
+++ util/log.ts
@@ -6,8 +6,8 @@ export const Log = {
   info: (msg: string, ...args: any[]): void => console.log(msg, ...args),
   warn: (msg: string, ...args: any[]): void => console.warn(msg, ...args),
   error: (msg: string, ...args: any[]): void => {
-    sentry.capture(new BLError(msg, args, "BConsoleError"));
     console.error(msg, ...args);
+    sentry.capture(new BLError(msg, args, "BConsoleError"));
   },
   // tslint:enable:no-console
 };
