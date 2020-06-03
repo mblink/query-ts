92a77fffc4aea2a32f0ab24ae372c27d30a0e51d
Justin Leider <justin@leiders.org>
2020-06-03 11:03:27 -0400

    Remove unused util files

--- src/util/parseBool.ts
+++
@@ -1,4 +0,0 @@
-import { none, Option, some } from "fp-ts/lib/Option";
-
-export const parseBool = (x: string): Option<boolean> =>
-  x === "true" ? some(true) : (x === "false" ? some(false) : none);
--- src/util/tap.ts
+++
@@ -1,4 +0,0 @@
-export const tap = <A>(f: (a: A) => any) => (a: A): A => {
-  f(a);
-  return a;
-};
