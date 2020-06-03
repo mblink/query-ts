ab029c64e25265dedde5844e3c0201e80db3311b
Justin Leider <justin@leiders.org>
2020-06-03 11:03:27 -0400

    Remove unused util files

--- src/util/updProp.ts
+++
@@ -1,3 +0,0 @@
-export function updProp<K extends PropertyKey>(k: K): <A extends Record<K, any>>(f: (v: A[K]) => A[K]) => (a: A) => A {
-  return <A extends Record<K, any>>(f: (v: A[K]) => A[K]) => (a: A): A => Object.assign({}, a, { [k]: f(a[k]) });
-}
